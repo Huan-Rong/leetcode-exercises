f615f896fa6d48630127484c9105dd4bc584c23e
Ian <zhenghuanrong@yeah.net>
2020-05-25 15:17:34 +0800

    提交 283 第一遍和第二遍练习

---
+++ src/main/java/leetcode/editor/cn/P283MoveZeroes.java
@@ -0,0 +1,123 @@
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。 
+//
+// 示例: 
+//
+// 输入: [0,1,0,3,12]
+//输出: [1,3,12,0,0] 
+//
+// 说明: 
+//
+// 
+// 必须在原数组上操作，不能拷贝额外的数组。 
+// 尽量减少操作次数。 
+// 
+// Related Topics 数组 双指针
+
+
+package leetcode.editor.cn;
+
+/**
+ * Java：移动零
+ */
+public class P283MoveZeroes {
+
+  public static void main(String[] args) {
+    Solution solution = new P283MoveZeroes().new Solution();
+  }
+
+  //leetcode submit region begin(Prohibit modification and deletion)
+  @SuppressWarnings("Duplicates")
+  class Solution {
+
+    public void moveZeroes(int[] nums) {
+      quickerLoop2ndTime(nums);
+    }
+
+    /******************第2遍20200525************************/
+    /**
+     * 第二种解法：一遍遍历法。
+     *
+     * 思想：借鉴快速排序思想，以 0 作为中间点。
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     */
+    private void quickerLoop2ndTime(int[] nums) {
+      for (int i = 0, j = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+          // 避免相同的非零数组元素进行交换。
+          if (i > j) {
+            nums[j] = nums[i];
+            nums[i] = 0;
+          }
+          j++;
+        }
+      }
+    }
+
+    /**
+     * 第一种解法：两遍遍历。
+     *
+     * <p>第一遍计算出非零元素的总数，并进行赋值操作。
+     * 第二遍将索引值为 [j, nums.length - 1] 的元素置为 0。
+     *
+     * 时间复杂度：O(n) 空间复杂度：O(1)
+     */
+    private void loop2ndTime(int[] nums) {
+      int j = 0;
+      for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+          nums[j++] = nums[i];
+        }
+      }
+
+      for (int i = j; i < nums.length; i++) {
+        nums[i] = 0;
+      }
+    }
+
+    /******************第1遍20200525************************/
+    /**
+     * 第二种解法：一遍遍历法。
+     *
+     * 思路：借助快速排序思想，将 0 作为中间点。
+     *
+     * 时间复杂度：O(n) 空间复杂度：O(1)
+     */
+    private void quickerLoop1stTime(int[] nums) {
+      for (int i = 0, j = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+          // 避免数组元素相同，且不为零时进行交换操作。
+          if (i > j) {
+            nums[j] = nums[i];
+            nums[i] = 0;
+          }
+          j++;
+        }
+      }
+    }
+
+    /**
+     * 第一种解法：两次遍历法。
+     *
+     * <p>第一遍：计算非零元素的个数，且在遍历过程中进行非零元素的复制。
+     * 第二遍：将剩余的元素全部置为 0。
+     *
+     * 时间复杂度：O(N)，如果数组元素全为 0，那么会执行 2N 次，时间复杂度还是 O(N)。 空间复杂度：O(1)
+     */
+    private void loop1stTime(int[] nums) {
+      int j = 0;
+      for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+          nums[j++] = nums[i];
+        }
+      }
+
+      for (int i = j; i < nums.length; i++) {
+        nums[i] = 0;
+      }
+    }
+  }
+  //leetcode submit region end(Prohibit modification and deletion)
+
+}
