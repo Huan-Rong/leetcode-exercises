b93ea262dac6d825ed7a6685e6097165d824b04b
Ian <zhenghuanrong@yeah.net>
2020-05-26 09:58:51 +0800

    提交 283 第三遍练习

--- src/main/java/leetcode/editor/cn/P283MoveZeroes.java
+++ src/main/java/leetcode/editor/cn/P283MoveZeroes.java
@@ -30,7 +30,57 @@ public class P283MoveZeroes {
   class Solution {
 
     public void moveZeroes(int[] nums) {
-      quickerLoop2ndTime(nums);
+      quickerLoop3rdTime(nums);
+    }
+
+    /******************第3遍20200526************************/
+    /**
+     * 第二种解法：一遍遍历法。
+     * 
+     * 这种解法其实是在优化第一种解法，因为两遍遍历是可以合为一遍遍历的。
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     */
+    private void quickerLoop3rdTime(int[] nums) {
+      for (int i = 0, j = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+          if (i>j) {
+            nums[j] = nums[i];
+            nums[i] = 0;
+          }
+          j++;
+        }
+      }
+    }
+    /**
+     * 第一种解法：两遍遍历法。
+     *
+     * <p>第一次遍历，求出非零元素的个数，且进行非零元素的复制。
+     * 第二次遍历，将索引值在 [j, nums.length - 1] 范围的元素置为 0 。
+     *
+     * <p>第一次遍历使用了双指针，j 指针指向下一个会被赋值的元素。
+     *
+     * <p>时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     *
+     * <p>测试用例：
+     *
+     * {@code [],[1],[0],[0,0,0],[1,2,0,2,43,0],[0,1,2,0]}
+     */
+    private void loop3rdTime(int[] nums) {
+      int j = 0;
+      for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+          if (i > j) {
+            nums[j] = nums[i];
+          }
+          j++;
+        }
+      }
+      for (int i = j; i < nums.length; i++) {
+        nums[i] = 0;
+      }
     }
 
     /******************第2遍20200525************************/
@@ -39,8 +89,7 @@ public class P283MoveZeroes {
      *
      * 思想：借鉴快速排序思想，以 0 作为中间点。
      *
-     * 时间复杂度：O(n)
-     * 空间复杂度：O(1)
+     * 时间复杂度：O(n) 空间复杂度：O(1)
      */
     private void quickerLoop2ndTime(int[] nums) {
       for (int i = 0, j = 0; i < nums.length; i++) {
