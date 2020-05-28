9bc17a57a622edb9aca09c3f96365eb01fb7cca9
Ian <zhenghuanrong@yeah.net>
2020-05-28 13:32:39 +0800

    提交 ListNode.java

---
+++ src/main/java/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,25 @@
+package leetcode.editor.cn;
+
+/**
+ * @description:
+ * @author: Ian Zheng
+ * @date: 2020-05-22
+ */
+public class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
